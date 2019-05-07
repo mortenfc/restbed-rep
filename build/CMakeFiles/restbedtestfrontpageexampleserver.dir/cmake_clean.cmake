f5e7e8ede817fd0f0a896c662694be6ac86ffdf4
mortenfc <mortenfjord@hotmail.com>
2019-05-07 17:15:27 +0200

    upload all

---
+++ build/CMakeFiles/restbedtestfrontpageexampleserver.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/restbedtestfrontpageexampleserver.dir/main.cpp.o"
+  "restbedtestfrontpageexampleserver.pdb"
+  "restbedtestfrontpageexampleserver"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/restbedtestfrontpageexampleserver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
